5a91a08e49c8e63452b89768e17d008b7d7cad72
quentinmel <112957970+quentinmel@users.noreply.github.com>
2024-02-01 17:33:50 +0100

    feat:save game json

---
+++ WpfApp1/MVVM/ViewModel/SaveChessboard.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1.MVVM.ViewModel
+{
+    internal class SaveChessboard
+    {
+        public static void SaveChessPieces(string[,] chessPieces)
+        {
+            // Convert the array to JSON
+            string jsonContent = JsonConvert.SerializeObject(chessPieces, Formatting.Indented);
+
+            MessageBox.Show(jsonContent);
+
+            // Save the JSON to a file in the project directory
+            DirectoryInfo directoryInfo = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+            string projectDirectory = directoryInfo.Parent.Parent.Parent.FullName;
+            string filePath = Path.Combine(projectDirectory, "ChessPieces.json");
+
+            File.WriteAllText(filePath, jsonContent);
+        }
+    }
+}
